0e0ae755690b3dd46011b6cb9b56d12b8b38e45d
Cory Boudreaux <cboud31@gmail.com>
2023-01-06 16:35:04 -0600

    Update die component properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
+
 import Die from "./components/Die";
+
+/**
+ * Challenge: Add conditional styling to the Die component
+ * so that if it's held (isHeld === true), its background color
+ * changes to a light green (#59E391)
+ *
+ * Remember: currently the Die component has no way of knowing
+ * if it's "held" or not.
+ */
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
@@ -8,18 +19,30 @@ function App() {
     const newDice = [];
 
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.floor(Math.random() * 6) + 1);
+      newDice.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        id: nanoid(),
+        isHeld: false,
+      });
     }
     return newDice;
   }
 
-  const diceElements = dice.map((die) => <Die value={die} />);
-  console.log(`Dice array: ${dice}`);
+  function handleClick() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die value={die.value} key={die.id} isHeld={die.isHeld} />
+  ));
 
   return (
     <div className="App">
       <main>
         <div className="dice-container">{diceElements}</div>
+        <button className="roll-dice" onClick={handleClick}>
+          <span>Roll Dice</span>
+        </button>
       </main>
     </div>
   );
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 const Die = (props) => {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
   return (
-    <div className="die">
+    <div className="die-face" style={styles}>
       <span>{props.value}</span>
     </div>
   );
